aa5b4de28c5cdfd1a9e717f733108cc6293c847e
Michael Sanabria <maicolsana12@gmail.com>
2022-02-19 01:45:33 -0500

    Add makepayment to one single route, instead of 3

---
+++ controllers/prepayment.js
@@ -0,0 +1,64 @@
+const config = require("../config");
+const User = require("../models/user");
+
+const epayco = require("epayco-sdk-node")({
+  apiKey: config.epayco.publicKey,
+  privateKey: config.epayco.privateKey,
+  lang: "ES",
+  test: true,
+});
+
+exports.createCardTokenAndCustomer = async (req, res) => {
+  const { decoded = {}, body: payment } = req;
+  //El objeto creditCardInfo lo recibe el body. A partir del req.body se construye el objeto creditCard
+  const creditCardInfo = {
+    "card[number]": payment.cardNumber,
+    "card[exp_year]": payment.expYear,
+    "card[exp_month]": payment.expMonth,
+    "card[cvc]": payment.CVC,
+  };
+
+  const { card, id, status } = await epayco.token.create(creditCardInfo);
+
+  const creditCard = {
+    expMonth: card.exp_month,
+    expYear: card.exp_year,
+    name: card.name,
+    mask: card.mask,
+    tokenId: id,
+  };
+
+  const findUser = await User.findById(decoded.id);
+
+  findUser.premium.creditCards =
+    findUser.premium.creditCards.concat(creditCard);
+
+  const updatedUser = await findUser.save();
+
+  ///////// Ahora creo el customer
+
+  const creditCards = updatedUser.premium.creditCards;
+
+  const userInfo = {
+    token_card: updatedUser?.premium?.creditCards?.[0]?.tokenId,
+    name: updatedUser.firstName,
+    last_name: updatedUser.lastName,
+    email: updatedUser.email,
+    default: true,
+  };
+
+  const { data } = await epayco.customers.create(userInfo);
+
+  const user = await User.findByIdAndUpdate(
+    decoded.id,
+    {
+      premium: {
+        creditCards,
+        customerId: data.customerId,
+      },
+    },
+    { new: true }
+  );
+
+  return user;
+};
